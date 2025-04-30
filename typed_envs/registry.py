1ba3f492a7aa92141261bbf84b261b3719db964b
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-29 23:59:48 -0400

    Update registry.py

--- typed_envs/registry.py
+++ typed_envs/registry.py
@@ -7,6 +7,13 @@ ENVIRONMENT: Final[EnvRegistry] = EnvRegistry({})
 _ENVIRONMENT_VARIABLES_SET_BY_USER: Final[EnvRegistry] = EnvRegistry({})
 _ENVIRONMENT_VARIABLES_USING_DEFAULTS: Final[EnvRegistry] = EnvRegistry({})
 
+def _register_new_env(name: VarName, instance: EnvironmentVariable) -> None:
+    ENVIRONMENT[name] = instance
+    if instance._using_default:
+        _ENVIRONMENT_VARIABLES_USING_DEFAULTS[name] = instance
+    else:
+        _ENVIRONMENT_VARIABLES_SET_BY_USER[name] = instance
+
 __all__ = [
     "ENVIRONMENT",
     "_ENVIRONMENT_VARIABLES_SET_BY_USER",
