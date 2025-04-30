900c3d8e796a7b25d3c79d233b93e7373d2d696f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-29 23:54:25 -0400

    Update registry.py

--- typed_envs/registry.py
+++ typed_envs/registry.py
@@ -3,9 +3,9 @@ from typing import Final
 from typed_envs.typing import EnvRegistry
 
 
-ENVIRONMENT: Final[EnvRegistry] = {}
-_ENVIRONMENT_VARIABLES_SET_BY_USER: Final[EnvRegistry] = {}
-_ENVIRONMENT_VARIABLES_USING_DEFAULTS: Final[EnvRegistry] = {}
+ENVIRONMENT: Final[EnvRegistry] = EnvRegistry({})
+_ENVIRONMENT_VARIABLES_SET_BY_USER: Final[EnvRegistry] = EnvRegistry({})
+_ENVIRONMENT_VARIABLES_USING_DEFAULTS: Final[EnvRegistry] = EnvRegistry({})
 
 __all__ = [
     "ENVIRONMENT",
