cc5ef46104ee3f88c81afdaf930c9688607822c5
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-04 15:11:24 -0400

    chore: drop support for python 3.6

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     author="BobTheBuidler",
     author_email="bobthebuidlerdefi@gmail.com",
     license="MIT",
+    python_requires=">=3.7",
     setup_requires=["setuptools_scm"],
     package_data={"typed_envs": ["py.typed"]},
     include_package_data=True,
