78d71b33f68c31501ad9632c06a88dccda9331f5
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-12-31 21:35:37 -0400

    Update typing.py

--- typed_envs/typing.py
+++ typed_envs/typing.py
@@ -1,12 +1,14 @@
 from typing import TYPE_CHECKING, Any, Callable, NewType
 
 if TYPE_CHECKING:
-    pass
+    from typed_envs._env_var import EnvironmentVariable
+
+    _EnvRegistry = dict[VarName, EnvironmentVariable]
 
 
 VarName = NewType("VarName", str)
 VarValue = NewType("VarValue", str)
 
-EnvRegistry = NewType("EnvRegistry", dict[VarName, "EnvironmentVariable"])
+EnvRegistry = NewType("EnvRegistry", "_EnvRegistry")
 
 StringConverter = Callable[[str], Any]
